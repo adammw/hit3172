f7f6a0b5adcc22b79b3f410deea749c532ec5bb6
Adam Malcontenti-Wilson <adman.com@gmail.com>
2012-08-09 14:55:54 +1000

    lab02: drawing++

--- lab02/core_exercises/drawing/src/main.cpp
+++ lab02/core_exercises/drawing/src/main.cpp
@@ -11,32 +11,55 @@
 
 using namespace std;
 
+/* TODO - Exercises
+ * 1. In your own words explain why all the fields in the ShapeDrawer program are private.
+ * 2. Point2D is a structure (value type) in C#. Review the material on the stack and heap and
+ * their relationship with value and reference types then answer the following question.
+ * In C#, why cant you use s.Position.X += dx to move the Shape? Explain what would occur at run
+ * time if the compiler allows this. Would this work correctly in Objective C or Java?
+ * What is the difference?
+ * 3. Add accessor methods or properties for X and Y in the Shape class. This will read and write the component values from the Shape's Position, and provide convenient access for modifying these values. (Make sure that the position field is initialised to a new object when the Shape is constructed.)
+ */
+
 int main() {
 	open_audio();
 	open_graphics_window("Blackjack", 800, 600);
 	load_default_colors();
 
-	Drawing* my_drawing = new Drawing;
+	Drawing *d = new Drawing();
 
-	Shape* my_shape = new Shape;
-	point2d pos = {10.0,10.0};
-	my_shape->set_color(ColorRed);
-	my_shape->set_position(pos);
-	my_shape->set_width(20);
-	my_shape->set_height(10);
+	// Create a shape
+	Shape *s = new Rectangle();
+	s->set_color(ColorRed);
+	s->set_position(random_screen_point());
+	s->set_width(200);
+	s->set_height(200);
 
-	cout << "Shape " << (my_shape->is_at(pos) ? "is" : "is NOT") << " at " << pos.x << "," << pos.y << endl;
+	// Add to drawing
+	d->add_shape(s);
 
-	pos.x = pos.y = 15;
+	// Create a shape
+	s = new Line();
+	s->set_color(ColorGreen);
+	s->set_position(random_screen_point());
+	s->set_width(100);
+	s->set_height(250);
 
-	cout << "Shape " << (my_shape->is_at(pos) ? "is" : "is NOT") << " at " << pos.x << "," << pos.y << endl;
+	// Add to drawing
+	d->add_shape(s);
 
-	pos.x = 30;
-	pos.y = 20;
+	// Create a shape
+	s = new Rectangle();
+	s->set_color(ColorBlue);
+	s->set_position(random_screen_point());
+	s->set_width(250);
+	s->set_height(100);
 
-	cout << "Shape " << (my_shape->is_at(pos) ? "is" : "is NOT") << " at " << pos.x << "," << pos.y << endl;
+	// Add to drawing
+	d->add_shape(s);
 
-	my_drawing->add_shape(my_shape);
+	// Forget s... its owned by d now
+	s = NULL;
 
 	do
 	{
@@ -44,14 +67,14 @@ int main() {
 
 		clear_screen(ColorWhite);
 
-		my_drawing->draw();
+		d->draw();
 
 		draw_framerate(0,0);
 
 		refresh_screen();
 	} while ( ! window_close_requested() );
 
-	delete my_shape;
+	delete d;
 
 	close_audio();
 
