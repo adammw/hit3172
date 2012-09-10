d2c8c09a2dd2c0a726ca7b1c34f61c27743649a4
Adam Malcontenti-Wilson <adman.com@gmail.com>
2012-09-10 15:29:45 +1000

    lab06: add extended comments to rpn-calc methods

--- lab06/core_exercises/rpn-calc/src/rpncalculator.cpp
+++ lab06/core_exercises/rpn-calc/src/rpncalculator.cpp
@@ -18,20 +18,38 @@ rpn_calculator::~rpn_calculator() {
 	// TODO Auto-generated destructor stub
 }
 
+/**
+ * Push a new value onto the operands stack
+ * @param value
+ */
 void rpn_calculator::push(int value) {
 	_operands.push_back(value);
 }
 
+/**
+ * Pop a value from the operands stack
+ * (removes the value from the stack and returns it)
+ * @return
+ */
 int rpn_calculator::pop() {
 	int value = _operands.back();
 	_operands.pop_back();
 	return value;
 }
 
+/**
+ * Peek at the answer (top of stack) without
+ * removing it from the stack
+ * @return
+ */
 int rpn_calculator::peek_answer() {
 	return _operands.back();
 }
 
+/**
+ * Add the last two operands together and
+ * push the result back onto the stack
+ */
 void rpn_calculator::add() {
 	int operand_1 = pop();
 	int operand_2 = pop();
@@ -39,6 +57,10 @@ void rpn_calculator::add() {
 	push(operand_2 + operand_1);
 }
 
+/**
+ * Subtract the second last operand from the last operand and
+ * push the result back onto the stack
+ */
 void rpn_calculator::subtract() {
 	int operand_1 = pop();
 	int operand_2 = pop();
@@ -46,6 +68,10 @@ void rpn_calculator::subtract() {
 	push(operand_2 - operand_1);
 }
 
+/**
+ * Multiply the last two operands together and
+ * push the result back onto the stack
+ */
 void rpn_calculator::multiply() {
 	int operand_1 = pop();
 	int operand_2 = pop();
@@ -53,6 +79,10 @@ void rpn_calculator::multiply() {
 	push(operand_2 * operand_1);
 }
 
+/**
+ * Divide the second last operand by the last operand and
+ * push the result back onto the stack
+ */
 void rpn_calculator::divide() {
 	int operand_1 = pop();
 	int operand_2 = pop();
@@ -60,6 +90,12 @@ void rpn_calculator::divide() {
 	push(operand_2 / operand_1);
 }
 
+/**
+ * Get the answer (result) from the top of the stack
+ * and remove it from the stack
+ * @see pop
+ * @return
+ */
 int rpn_calculator::answer() {
 	return pop();
 }
