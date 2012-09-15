47b09e0b1fcd7d1a6751459c7f1f84166323b25b
Adam Malcontenti-Wilson <adman.com@gmail.com>
2012-09-16 00:12:01 +1000

    Update main
    * Use C++ iostream rather than C stdio.h
    * Create a GameObject and print it's properties

--- swin-adventure/src/main.cpp
+++ swin-adventure/src/main.cpp
@@ -1,20 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 #include "config.h"
 
-#include "Identifiable.h"
+#include "GameObject.h"
  
+using namespace std;
 using namespace swinadventure;
 
 int main (int argc, char *argv[])
 {
-  fprintf(stdout,"%s Version %d.%d\n",
-            argv[0],
-            SWINADVENTURE_VERSION_MAJOR,
-            SWINADVENTURE_VERSION_MINOR);
+	cout << argv[0] << " Version " << SWINADVENTURE_VERSION_MAJOR << '.' << SWINADVENTURE_VERSION_MINOR << endl;
 
-  std::string idents[2] = {"gem", "ruby"};
-  Identifiable* identObj = new Identifiable(idents, 2);
-
-  return 0;
+	string idents[2] = {"gem", "ruby"};
+	GameObject* gameObj = new GameObject(idents, 2, "blood-red ruby", "The small blood-red ruby is dulled from the years of wear");
+	cout << "Game Object name: " << std::string(gameObj->get_name()).c_str() << endl;
+	cout << "Game Object short desc: " << gameObj->get_short_description() << endl;
+	cout << "Game Object long desc: " << gameObj->get_full_description() << endl;
+	return 0;
 }
