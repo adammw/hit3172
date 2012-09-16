39fd7e7cd465280aadb290093f833d28bb3d7f6f
Adam Malcontenti-Wilson <adman.com@gmail.com>
2012-09-16 18:33:10 +1000

    Change main to show a sample Player description

--- swin-adventure/src/main.cpp
+++ swin-adventure/src/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "config.h"
 
-#include "GameObject.h"
+#include "Player.h"
+#include "Inventory.h"
  
 using namespace std;
 using namespace swinadventure;
@@ -10,10 +11,27 @@ int main (int argc, char *argv[])
 {
 	cout << argv[0] << " Version " << SWINADVENTURE_VERSION_MAJOR << '.' << SWINADVENTURE_VERSION_MINOR << endl;
 
-	string idents[2] = {"gem", "ruby"};
-	GameObject* gameObj = new GameObject(idents, 2, "blood-red ruby", "The small blood-red ruby is dulled from the years of wear");
-	cout << "Game Object name: " << std::string(gameObj->get_name()).c_str() << endl;
-	cout << "Game Object short desc: " << gameObj->get_short_description() << endl;
-	cout << "Game Object long desc: " << gameObj->get_full_description() << endl;
+	Player* p = new Player("Test Player", "A player used for testing");
+	Inventory* pi = p->get_inventory();
+
+	// Create some sample items
+	std::string idents1[2] = {"gem", "ruby"};
+	Item* i = new Item(idents1, 2, "small blood-red ruby", "The small blood-red ruby is dulled from the years of wear");
+	pi->put(i);
+
+	std::string idents2[2] = {"crowbar", "bar"};
+	i = new Item(idents2, 2, "large steel crowbar", "The crowbar has signs of rust and heavy wear, but still works fine");
+	pi->put(i);
+
+	std::string idents3[1] = {"hammer"};
+	i = new Item(idents3, 1, "cheap nasty hammer", "The hammer is very new - still has it's original sale stickers on it, but looks like it might break");
+	pi->put(i);
+
+	// Locate items
+	cout << "=== Player description ===" << endl;
+	cout << p->get_full_description() << endl;
+
+	// Free objects
+	delete p;
 	return 0;
 }
